45ccff13388e4ab22c8d79c57ca7ba8c10b4d9f8
aryagiat <arya.giat@gmail.com>
2020-12-27 10:24:50 +0800

    Added displayBoard() method

--- Sudoku.java
+++ Sudoku.java
@@ -1,2 +1,24 @@
 class Sudoku {
+    private static int[][] board = new int[9][9]; 
+    
+    public static void main(String[] args){
+        displayBoard();
+    }
+
+    public static void displayBoard(){
+        // Display the board.
+        for (int r  = 0; r < 9; ++r){
+            if (r % 3 == 0){
+                System.out.println("-------------------------");
+            }
+            for (int c = 0; c < 9; ++c){
+                if (c % 3 == 0){
+                    System.out.print("| ");
+                }
+                System.out.print(board[r][c] + " ");
+            }
+            System.out.print("|\n");
+        }
+        System.out.println("-------------------------");
+    }
 }

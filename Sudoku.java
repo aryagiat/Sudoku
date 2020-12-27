76a0b3e3f408544de79138d3e7d9239fc4d5a31e
aryagiat <arya.giat@gmail.com>
2020-12-27 20:06:59 +0800

    Added solver

--- Sudoku.java
+++ Sudoku.java
@@ -1,6 +1,5 @@
 class Sudoku {
     // The board data structure is a 2D array
-    /*
     // Incomplete board
     public static int[][] board = 
                 {{5, 3, 0, 0, 7, 0, 0, 0, 0},
@@ -12,8 +11,7 @@ class Sudoku {
                  {0, 6, 0, 0, 0, 0, 2, 8, 0},
                  {0, 0, 0, 4, 1, 9, 0, 0, 5},
                  {0, 0, 0, 0, 8, 0, 0, 7, 9}};
-    */
-
+/*
     // Completed board
     public static int[][] board =
                 {{5, 3, 4, 6, 7, 8, 9, 1, 2},
@@ -25,7 +23,8 @@ class Sudoku {
                  {9, 6, 1, 5, 3, 7, 2, 8, 4},
                  {2, 8, 7, 4, 1, 9, 6, 3, 5},
                  {3, 4, 5, 2, 8, 6, 1, 7, 9}};
-    
+*/    
+
     public static void main(String[] args){
         displayBoard();
         System.out.println("Row: " + checkAllRow());
--- SudokuSolver.java
+++ SudokuSolver.java
@@ -2,23 +2,63 @@ class SudokuSolver{
     public static Sudoku grid = new Sudoku();
 
     public static void main(String[] args){
-        int[] find = findEmpty();
-        if (find[0] == 9 && find[1] == 9)
+        if (solve(0, 0)){
             grid.displayBoard();
-
+        } else {
+            System.out.println("No Solution");
+        }
     }
 
-    public static int[] findEmpty(){
-        int[] coord = {9, 9};
+    public static boolean solve(int row, int col){
+        if (row == 8 && col == 9)
+            return true;
+
+        if (col == 9){
+            ++row;
+            col = 0;
+        }
+
+
+        if (grid.board[row][col] != 0)
+           return solve(row, col + 1);
+
+        for (int num = 1; num < 10; ++num){
+            if (isValid(num, row, col)){
+        //System.out.println("Row: " + row + " Col: " + col + "Num : " + num);
+                grid.board[row][col] = num;
+
+                if (solve(row, col + 1))
+                    return true;
+            }
+
+            grid.board[row][col] = 0;
+        }
+        return false;
+    }
+
+    public static boolean isValid(int num, int row, int col){
+        // check whether inputting num to row and col is valid
+        // Check row
+        for (int c = 0; c < 9; ++c){
+            if (grid.board[row][c] == num && col != c)
+                return false;
+        }
+
+        // Check col
         for (int r = 0; r < 9; ++r){
-            for (int c = 0; c < 9; ++c){
-                if (grid.board[r][c] == 0){
-                    coord[0] = r;
-                    coord[1] = c;
-                    return coord;
-                }
+            if (grid.board[r][col] == num && row != r)
+                return false;
+        }
+
+        // Check 3x3 box
+        int startRow = row - row % 3, startCol = col - col % 3;
+        for (int r = 0; r < 3; ++r){
+            for (int c = 0; c < 3; ++c){
+                if (grid.board[r + startRow][c + startCol] == num)
+                    return false;
             }
         }
-        return coord;
+
+        return true;
     }
 }
